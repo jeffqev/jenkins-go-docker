c7e17008bc0d34b458764d0159f419a3f0513b8d
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2020-08-26 20:05:32 -0500

    commit 3

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func routers() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Github WebHook commit 2")
+		fmt.Fprintf(w, "Github WebHook commit 3")
 	})
 
 	http.HandleFunc("/hola", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "HOla Probando")
+		fmt.Fprintf(w, "HOla Probando commit 3")
 	})
 
 }
